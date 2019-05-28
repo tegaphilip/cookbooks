b9ba4392917be19109614257d9786efd4dbeb385
tega <tega.philip@gmail.com>
2019-05-28 10:35:31 +0200

    Updated upload max size to 32MB on server

--- php56_apache/recipes/ini.rb
+++ php56_apache/recipes/ini.rb
@@ -10,10 +10,8 @@ ini_file = node[:php_ini]
 if File.exist?(ini_file)
 	file = File.open(ini_file, "rb")
 	contents = file.read
-	contents = contents.gsub('post_max_size = 8M', ';')
-	contents = contents.gsub('post_max_size = 20M', ';')
-	contents = contents.gsub('upload_max_filesize = 2M', ';')
-	contents = contents.gsub('upload_max_filesize = 20M', ';')
+	contents = contents.gsub('post_max_size', ';')
+	contents = contents.gsub('upload_max_filesize', ';')
 
 	contents = contents + "\n" + 'post_max_size = 32M'
 	contents = contents + "\n" + 'upload_max_filesize = 32M'
