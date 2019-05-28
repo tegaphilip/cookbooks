91f391725858440b82e32e8aaf74bc73762b92ae
tega <tega.philip@gmail.com>
2019-05-28 11:10:26 +0200

    Increased max execution and input time to 300seconds

--- php56_apache/recipes/ini.rb
+++ php56_apache/recipes/ini.rb
@@ -12,7 +12,11 @@ if File.exist?(ini_file)
 	contents = file.read
 	contents = contents.gsub('post_max_size', ';')
 	contents = contents.gsub('upload_max_filesize', ';')
+	contents = contents.gsub('max_execution_time', ';')
+	contents = contents.gsub('max_input_time', ';')
 
+	contents = contents + "\n" + 'max_execution_time = 300'
+	contents = contents + "\n" + 'max_input_time = 300'
 	contents = contents + "\n" + 'post_max_size = 32M'
 	contents = contents + "\n" + 'upload_max_filesize = 32M'
 
